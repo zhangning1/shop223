5d397c9e8738af7dee071fe9ad1fb4c6c923f441
zhangning <1847786689@qq.com>
2017-06-23 20:06:26 +0800

    second

--- src/shop/utils/Send.java
+++ src/shop/utils/Send.java
@@ -19,7 +19,7 @@ public class Send {
 	 // 发件人的 邮箱 和 密码（替换为自己的邮箱和密码）
     // PS: 某些邮箱服务器为了增加邮箱本身密码的安全性，给 SMTP 客户端设置了独立密码（有的邮箱称为“授权码”）, 
     //     对于开启了独立密码的邮箱, 这里的邮箱密码必需使用这个独立密码（授权码）。
-    public static String myEmailAccount = "ningzh987@126.com";
+    public static String myEmailAccount = "@126.com";
     public static String myEmailPassword = "19960715ning";
 
     // 发件人邮箱的 SMTP 服务器地址, 必须准确, 不同邮件服务器地址不同, 一般(只是一般, 绝非绝对)格式为: smtp.xxx.com
